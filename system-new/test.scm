a6d40387f7ac11ed7aa950dc8785c0555265d0c6
retropikzel <retropikzel@iki.fi>
2025-10-25 05:44:32 +0300

    Fixing repo

---
+++ system-new/test.scm
@@ -0,0 +1,25 @@
+(import (scheme base)
+        (scheme write)
+        (scheme file)
+        (scheme process-context)
+        (foreign c)
+        (retropikzel system)
+        (srfi 64))
+
+(test-begin "foreign-c-system")
+
+(define testfile "/tmp/foreign-c-system-test.txt")
+
+(define exit-code1 (system (apply string-append `("echo \"Hello\" > " ,testfile))))
+
+(test-assert (= exit-code1 0))
+
+(define text (with-input-from-file testfile (lambda () (read-line))))
+
+(test-assert (string=? text "Hello"))
+
+(define exit-code2 (system "no-such-command"))
+
+(test-assert (not (= exit-code1 0)))
+
+(test-end "foreign-c-system")
